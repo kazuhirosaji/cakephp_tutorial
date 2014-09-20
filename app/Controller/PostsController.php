05c3218dc7f5b701b27f494363e288a6e62623e5
kazuhirosaji <kazuhirosaji@gmail.com>
2014-09-20 05:17:31 -0700

    impl add post function

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -17,4 +17,16 @@ class PostsController extends AppController {
 		}
 		$this->set('post', $post);
 	}
+
+	public function add() {
+		if ($this->request->is('post')) {
+			$this->Post->create();
+			pr($this->request->$data); // for debug
+			if ($this->Post->save($this->request->data)) {
+				$this->Session->setFlash(__('Your post has been saved.'));
+				return $this->redirect(array('action' => 'index'));
+			}
+			$this->Session->setFlash(__('Unable to add your post.'));
+		}
+	}
 }
