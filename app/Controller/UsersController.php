50ea816a77965101442f6fd4ab6412bd5932230e
kazuhirosaji <kazuhirosaji@gmail.com>
2014-09-30 07:25:18 -0700

    fix bugs

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -7,7 +7,7 @@ class UsersController extends AppController {
 
 	public function index() {
 		$this->User->recursive = 0;
-		$this->set('users', $this->paginate())
+		$this->set('users', $this->paginate());
 	}
 
 	public function view($id = null) {
@@ -16,6 +16,18 @@ class UsersController extends AppController {
 			throw new NotFoundException(__('Invalid user'));
 		}
 		$this->set('user', $this->User->read(null, $id));
+	}
+
+	public function add() {
+		if ($this->request->is('post')) {
+			$this->User->create();
+			if($this->User->save($this->request->data)) {
+				$this->Session->setFlash(__('The user has been saved'));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The user could not be saved. Please, try again.'));
+			}
+		}
 	}
 
 	public function edit($id = null) {
